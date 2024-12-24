9574e21fc86ae64a72e2bc7945689822e6e53997
nam2626 <nam2626@naver.com>
2024-12-24 10:13:49 +0900

    import, export

---
+++ step00/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import {sum, minus} from './func/math'
+
+function App() {
+  //값 비교(Primitive Type)
+  console.log(-0 === +0);
+  console.log(Object.is(-0, +0));
+
+  console.log(NaN === NaN);
+  console.log(Object.is(NaN, NaN));
+
+  //문자열 비교 - Primitive Type
+  let str1 = 'hello';
+  let str2 = 'hello';
+  console.log(str1 === str2);
+  console.log('-----------------');
+  //객체 비교 - Reference Type
+  let obj1 = {name : '홍길동'};
+  let obj2 = {name : '홍길동'};
+  console.log(obj1 == obj2);
+  console.log(obj1 === obj2);
+  console.log(Object.is(obj1, obj2));
+
+  obj2 = obj1;
+  console.log(obj1 == obj2);
+  console.log(obj1 === obj2);
+  console.log(Object.is(obj1, obj2));
+
+  console.log('-----------------');
+  console.log(sum(10,20));
+  console.log(minus(10,20));
+
+  return (
+    <div className="App">
+      <h1>리액트 테스트</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ step00/src/func/math.js
@@ -0,0 +1,10 @@
+//덧셈 뺄셈 함수
+function sum(a, b) {
+    return a + b;
+}
+
+// export const minus = (a, b) => a - b;
+const minus = (a, b) => a - b;
+
+export {sum, minus};
+
