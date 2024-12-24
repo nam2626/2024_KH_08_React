1416a161a2365c9222f6c713e21897ac56bb33d9
nam2626 <nam2626@naver.com>
2024-12-24 12:13:14 +0900

    스타일 적용

--- jsx/src/components/ComponentEx.js
+++ jsx/src/components/ComponentEx.js
@@ -1,3 +1,4 @@
+import './../css/style.css'
 export default function ComponentEx() {
     const func = () => alert('hello');
     //이벤트 처리 방법
@@ -5,6 +6,7 @@ export default function ComponentEx() {
 
     const items = ['item1', 'item2', 'item3'];
 
+    const style = {color:'red', fontSize:'20px',backgroundColor:'yellow'};
     return (
         <div>
             <h2>태그는 반드시 닫아야 됨, 단일태그도 포함</h2>
@@ -16,8 +18,21 @@ export default function ComponentEx() {
             <hr />
             <h2>반복적인 UI 생성</h2>
             <ul>
-                {items.map((item,idx) => <li key={idx}>{item}</li>)}    
-            </ul>            
+                {items.map((item,idx) => <li key={idx} className='bold'>{item}</li>)}    
+            </ul>
+            <hr />
+            <div style={{color:'red', fontSize:'20px',backgroundColor:'yellow'}}>
+                인라인 스타일 적용 방법
+            </div>            
+            <div style={style}>스타일 적용방법</div>
         </div>
     );
 }
+//JSX 규칙
+// 1. 반드시 부모요소 태그 하나로 감싸야 된다.
+// 2. JSX 안에서 자바스크립트 표현식을 쓸려면 {}로 묶어야 됨
+// 3. {} 내에서는 반복문이랑 if문을 사용하지 않음, 필요시 삼항연산자는 사용 가능
+// 4. CSS 적용시에도 카멜 표기법으로 작성해야함 (background-color —> backgroundColor)
+// 5. JSX의 속성은 DOM 속성명(javascript 속성)을 따른다 → class 를 className으로 사용
+// 6. 반드시 닫는 태그를 써야됨
+// 7. 반드시 ‘’ “”로 묶지 않음
