3483d7581d213cfe21475c6e6e33550846f7848f
nam2626 <nam2626@naver.com>
2024-12-24 11:18:48 +0900

    JSX 적용

--- jsx/src/components/NoJSX.js
+++ jsx/src/components/NoJSX.js
@@ -1,6 +1,11 @@
 import React from 'react';
 export default function NoJSX() {
-    return React.createElement('h3', null, 'Hello React App');
+    // return React.createElement('h3', null, 'Hello React App');
+    return (
+        <div>
+            <h3>Hello React App</h3>
+        </div>
+    );
 }
 
 
