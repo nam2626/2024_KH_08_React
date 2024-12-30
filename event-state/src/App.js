b1a2d6163e240bdcc28541d0c7e7d7d3dba2cb8b
nam2626 <nam2626@naver.com>
2024-12-30 17:26:47 +0900

    이벤트, 상태값

--- event-state/src/App.js
+++ event-state/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import TimeView from './components/TimeView';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div className="App">
       <TimeView/>
+      <hr/>
+      <LoginForm/>
     </div>
   );
 }
--- event-state/src/components/LoginForm.js
+++ event-state/src/components/LoginForm.js
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 export default () => {
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
@@ -9,17 +11,20 @@ export default () => {
     const updatePassword = (e) => {
         setPassword((current) =>{
             console.log('바뀌기 전 값 : ', current,'바뀐 값 : ', e.target.value);
-            return e.target.value;
+            return e.target.value; //바뀔값
         });
     }
 
     return (
         <div>
 			<h1>DefaultForm</h1>
-			<form method='get' >
-				<input type='text' placeholder='아이디' />
+			<form method='get' onSubmit={(e) => {
+                e.preventDefault();
+                console.log('아이디 : ', id, '비밀번호 : ', password);
+            }}>
+				<input type='text' placeholder='아이디' onKeyUp={updateId} />
 				<br />
-				<input type='password' placeholder='비밀번호'  />
+				<input type='password' placeholder='비밀번호' onChange={updatePassword} />
 				<br />
 				<input type='submit' value='로그인' />
 			</form>
