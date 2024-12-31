02f5ea076d08e213cdda88b245daadf9276f7b52
nam2626 <nam2626@naver.com>
2024-12-31 10:39:03 +0900

    hook

--- event-state/src/App.js
+++ event-state/src/App.js
@@ -4,6 +4,7 @@ import TimeView from './components/TimeView';
 import LoginForm from './components/LoginForm';
 import ColorToggle from './components/ColorToggle';
 import Counter from './components/Counter';
+import AlertState from './components/AlertState';
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
       <ColorToggle/>
       <hr/>
       <Counter/>
+      <hr/>
+      <AlertState/>
     </div>
   );
 }
