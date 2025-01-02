da63492e4a8e7cdf8c117f78c47c97ee2948de6f
nam2626 <nam2626@naver.com>
2025-01-02 10:13:55 +0900

    리스트에 숫자 추가후 출력

--- hook-ex/src/components/MemoEx.js
+++ hook-ex/src/components/MemoEx.js
@@ -1,10 +1,21 @@
+import { useRef, useState } from 'react';
 export default function MemoEx() {
-
-
+    const [list, setList] = useState([]);
+    const input = useRef(null);
+    const addNumber = () => {
+        setList([...list, input.current.value]);
+    }
+    const getAverage = (arr) => {
+        console.log("평균 계산중.....");
+        //평균을 구해서 리턴
+    }
     return (<>
         <h2>useMemo 테스트</h2>
-        <p> / 평균 : {0}  </p>
-        <p><input type="number" /><button>숫자 추가</button></p>
+        <p>{list.join(', ')} / 평균 : {0}  </p>
+        <p>
+            <input type="number" ref={input} />
+            <button onClick={addNumber}>숫자 추가</button>
+        </p>
         <p><button>테스트</button></p>
     </>);   
 }
