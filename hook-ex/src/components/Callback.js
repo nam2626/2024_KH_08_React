3d9aad0ffcb021ccec75151b21fb982de9bb1abb
nam2626 <nam2626@naver.com>
2025-01-02 11:42:09 +0900

    callback 적용

--- hook-ex/src/components/Callback.js
+++ hook-ex/src/components/Callback.js
@@ -1,9 +1,17 @@
+import { useCallback, useRef } from "react";
+
 export default () => {
+    const area = useRef(null);
+    const changeColor =useCallback((e) => {
+        console.log(e.target.value);
+        area.current.style.backgroundColor = e.target.value;
+    }, []);
+
     return (
         <div>
             <h2>useCallback 테스트</h2>
-            <div style={{width: '200px', height: '200px', border: '1px solid black', margin: '10px auto'}}></div>
-            <input type="color" />
+            <div style={{width: '200px', height: '200px', border: '1px solid black', margin: '10px auto'}} ref={area}></div>
+            <input type="color" onChange={changeColor}/>
         </div>
     );
 }
