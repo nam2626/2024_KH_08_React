c2b2aeee5c7b571e33fca40ca7a5306d04117e0d
nam2626 <nam2626@naver.com>
2025-01-02 10:58:29 +0900

    memo 테스트

--- hook-ex/src/components/MemoEx.js
+++ hook-ex/src/components/MemoEx.js
@@ -1,21 +1,27 @@
-import { useRef, useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 export default function MemoEx() {
     const [list, setList] = useState([]);
     const input = useRef(null);
     const addNumber = () => {
-        setList([...list, input.current.value]);
+        setList([...list, Number(input.current.value)]);
     }
     const getAverage = (arr) => {
         console.log("평균 계산중.....");
+        if(arr.length === 0) return 0;
         //평균을 구해서 리턴
+        return arr.reduce((pre, cur) => pre + cur, 0) / arr.length;
     }
+    const getAverageMemo =  useMemo(() => getAverage(list), [list]);
+
+    const [temp, setTemp] = useState(0);
+
     return (<>
         <h2>useMemo 테스트</h2>
-        <p>{list.join(', ')} / 평균 : {0}  </p>
+        <p>{list.join(', ')} / 평균 : {getAverageMemo}  </p>
         <p>
             <input type="number" ref={input} />
             <button onClick={addNumber}>숫자 추가</button>
         </p>
-        <p><button>테스트</button></p>
+        <p><button onClick={() => setTemp(temp + 1)}>테스트</button></p>
     </>);   
 }
