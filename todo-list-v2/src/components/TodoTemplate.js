1f78d22d4b11d6b6d3bf063f15266f2ddcc3e2ea
nam2626 <nam2626@naver.com>
2025-01-03 15:12:13 +0900

    reducer 생성

---
+++ todo-list-v2/src/components/TodoTemplate.js
@@ -0,0 +1,54 @@
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import { useCallback, useReducer, useRef, useState } from "react";
+const makeTodo = () => {
+    const todo = [];
+    for (let i = 1; i <= 5; i++) {
+        todo.push({id: i, text: `${i}번째 할일`, done: false});
+    }
+    return todo;
+}
+function reducer(state, action) {
+    switch (action.type) {
+        
+    }
+    return state;
+}
+
+export default function TodoTemplate() {
+    const [todoList, dispatch] = useReducer(reducer,{list:makeTodo(),id:6});
+    
+    const deleteTodo = useCallback((id) => {
+        //id 값을 가지고 todoList에 있는 값을 제거
+        setTodoList((todos) => {
+            return todos.filter((todo) => todo.id !== id);
+        });
+    },[]);
+
+    const updateTodo = useCallback((id) => {
+        //id에 가지고 todoList에 있는 id에 해당하는 done 값을 반대로 변경
+        setTodoList((todos) => {
+            return todos.map((todo) => {
+                if (todo.id === id) {
+                    return {...todo, done: !todo.done};
+                }
+                return todo;
+            });
+        });
+    },[]);
+
+    let id = useRef(6);
+    const addTodo = useCallback((text) => {
+        setTodoList((todos) => {
+            return [...todos, {id: id.current++, text, done: false}];
+        });
+    },[]);
+
+    return (
+        <div>
+            <h2>Todo List</h2>
+            <TodoForm addTodo={addTodo}/>
+            <TodoList todoList={todoList} deleteTodo={deleteTodo} updateTodo={updateTodo}/>
+        </div>
+    );
+}
