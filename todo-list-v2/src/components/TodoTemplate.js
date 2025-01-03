3aef4b47540e8f5cbff3d1bbf0a42bb958f62ed5
nam2626 <nam2626@naver.com>
2025-01-03 16:06:48 +0900

    수정

--- todo-list-v2/src/components/TodoTemplate.js
+++ todo-list-v2/src/components/TodoTemplate.js
@@ -1,6 +1,6 @@
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
-import { useCallback, useReducer, useRef, useState } from "react";
+import { useCallback, useReducer } from "react";
 const makeTodo = () => {
     const todo = [];
     for (let i = 1; i <= 5; i++) {
@@ -12,6 +12,15 @@ function reducer(state, action) {
     switch (action.type) {
         case 'DELETE':
             return {...state, list: state.list.filter((todo) => todo.id !== action.payload)};
+        case 'UPDATE':
+            return {...state,list: state.list.map((todo) => {
+                if (todo.id === action.payload) {
+                    return {...todo, done: !todo.done};
+                }
+                return todo;
+            })};
+        case 'ADD':
+            return {...state, list: state.list.concat({id: state.id++, text: action.payload, done: false})};
     }
     return state;
 }
@@ -24,29 +33,18 @@ export default function TodoTemplate() {
     },[]);
 
     const updateTodo = useCallback((id) => {
-        //id에 가지고 todoList에 있는 id에 해당하는 done 값을 반대로 변경
-        setTodoList((todos) => {
-            return todos.map((todo) => {
-                if (todo.id === id) {
-                    return {...todo, done: !todo.done};
-                }
-                return todo;
-            });
-        });
+        dispatch({type: 'UPDATE', payload: id});
     },[]);
 
-    let id = useRef(6);
     const addTodo = useCallback((text) => {
-        setTodoList((todos) => {
-            return [...todos, {id: id.current++, text, done: false}];
-        });
+        dispatch({type: 'ADD', payload: text});
     },[]);
 
     return (
         <div>
             <h2>Todo List</h2>
             <TodoForm addTodo={addTodo}/>
-            <TodoList todoList={todoList} deleteTodo={deleteTodo} updateTodo={updateTodo}/>
+            <TodoList todoList={todoList.list} deleteTodo={deleteTodo} updateTodo={updateTodo}/>
         </div>
     );
 }
