782829d17ef53a378160722c99f4166f8cf49964
nam2626 <nam2626@naver.com>
2025-01-06 11:11:07 +0900

    index 수정

--- redux-ex/src/store/store.js
+++ redux-ex/src/store/store.js
@@ -2,6 +2,7 @@ import {configureStore} from '@reduxjs/toolkit'
 import counterReducer  from './counterSlice';
 
 import storage  from 'redux-persist/lib/storage';
+import {persistStore, persistReducer} from 'redux-persist';
 
 //로컬 스토리지에 저장하는 방식
 //redux-persist를 설치해서 사용
@@ -13,7 +14,7 @@ const persistConfig = {
 }
 
 //만든 슬라이스, 리듀서를 등록
-const persistedReducer = persistedReducer(persistConfig, counterReducer);
+const persistedReducer = persistReducer(persistConfig, counterReducer);
 
 
 //빈 저장소 만들기
