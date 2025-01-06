d93beb81f6d4e11aaeb0d95016620fe110ea5136
nam2626 <nam2626@naver.com>
2025-01-06 10:16:49 +0900

    컴포넌트 생성

---
+++ redux-ex/src/components/Counter.js
@@ -0,0 +1,12 @@
+export default function Counter() {
+  retrun (
+    <div>
+      <h2>redux 예제</h2>
+      <input type="text" /><button>증가</button>
+      <hr/>
+      <button>+</button>
+      <span>{0}</span>
+      <button>-</button>
+    </div>
+  );
+}
--- redux-ex/src/store/store.js
+++ redux-ex/src/store/store.js
@@ -1,6 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit'
+import counterReducer  from './counterSlice';
 
 //빈 저장소 만들기
 export default configureStore({
-  reducer: {}
+  reducer: {
+    counter : counterReducer
+  }
 });
