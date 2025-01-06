d96d0e88439e89a7440b0ac98b17e5d4e6e6a696
nam2626 <nam2626@naver.com>
2025-01-06 10:57:39 +0900

    기능 완성

--- redux-ex/src/components/Counter.js
+++ redux-ex/src/components/Counter.js
@@ -1,14 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "../store/counterSlice";
+import { decrement, increment, incrementByAmount } from "../store/counterSlice";
+import { useRef } from "react";
 
 export default function Counter() {
   const count = useSelector(state => state.counter.value);
   const dispatch = useDispatch();
+  const txt = useRef(null);
 
   return (
     <div>
       <h2>redux 예제</h2>
-      <input type="text" /><button>증가</button>
+      <input type="text" ref={txt}/><button onClick={() => dispatch(incrementByAmount(Number(txt.current.value)))}>증가</button>
       <hr/>
       <button onClick={() => dispatch(increment())}>+</button>
       <span>{count}</span>
