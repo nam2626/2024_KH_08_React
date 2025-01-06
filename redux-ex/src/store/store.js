26b15e2c8d87e79f152b92ff9d6b909ab2604f82
nam2626 <nam2626@naver.com>
2025-01-06 11:07:53 +0900

    persist 적용

--- redux-ex/src/store/store.js
+++ redux-ex/src/store/store.js
@@ -1,9 +1,26 @@
 import {configureStore} from '@reduxjs/toolkit'
 import counterReducer  from './counterSlice';
 
+import storage  from 'redux-persist/lib/storage';
+
+//로컬 스토리지에 저장하는 방식
+//redux-persist를 설치해서 사용
+
+//로컬 스토리지 셋팅
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+//만든 슬라이스, 리듀서를 등록
+const persistedReducer = persistedReducer(persistConfig, counterReducer);
+
+
 //빈 저장소 만들기
-export default configureStore({
+export const store = configureStore({
   reducer: {
-    counter : counterReducer
+    counter : persistedReducer
   }
 });
+
+export const persistor = persistStore(store);
