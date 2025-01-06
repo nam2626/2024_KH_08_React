1388e7228bb14d31731f8e5c668d7bf7fa1141e1
nam2626 <nam2626@naver.com>
2025-01-06 10:43:54 +0900

    리덕스 적용

--- redux-ex/src/components/Counter.js
+++ redux-ex/src/components/Counter.js
@@ -1,12 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../store/counterSlice";
+
 export default function Counter() {
+  const count = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
+
   retrun (
     <div>
       <h2>redux 예제</h2>
       <input type="text" /><button>증가</button>
       <hr/>
-      <button>+</button>
-      <span>{0}</span>
-      <button>-</button>
+      <button onClick={dispatch(increment())}>+</button>
+      <span>{count}</span>
+      <button onClick={dispatch(decrement())}>-</button>
     </div>
   );
 }
