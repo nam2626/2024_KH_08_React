b2e5e6b66af83ad2e5ff60d7376959f37e56e69d
nam2626 <nam2626@naver.com>
2025-01-08 15:06:42 +0900

    로그인 이벤트 처리

--- member-mange-app/src/page/Login.js
+++ member-mange-app/src/page/Login.js
@@ -1,11 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const navigate = useNavigate();
+
+  const login = () => {
+    navigate('/home');
+  }
 
   return (
     <div>
       <h2>로그인</h2>     
       <input type="id" placeholder="아이디"/><br/>
       <input type="password" placeholder="비밀번호"/><br/>
-      <button>로그인</button>
+      <button onClick={login}>로그인</button>
     </div>
   )
 }
