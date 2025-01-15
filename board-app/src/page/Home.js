339e215d423b9aea300da382807f7d77b094faf0
nam2626 <nam2626@naver.com>
2025-01-15 17:12:40 +0900

    문제 업데이트

--- board-app/src/page/Home.js
+++ board-app/src/page/Home.js
@@ -2,19 +2,10 @@ import { jwtDecode } from "jwt-decode";
 import { useSelector } from "react-redux";
 
 export default function Home() {
-  const user = useSelector(state => state.member.value);
-
-  let id = 'quest';
-
-  if(user.token){
-    const decodeToken = jwtDecode(user.token);
-    id = decodeToken.sub;
-  }
   
   return (
     <div>
       <h2>게시글 목록을 출력하는 페이지 입니다.</h2>
-      <p>{id} 님이 로그인하셨습니다.</p>
     </div>
   );
 }
